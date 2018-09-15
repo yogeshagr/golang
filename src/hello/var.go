6b325b0ae9e022988ef7ea7b79d4f6bd036292d6
yogeshagr <yogeshiiith@gmail.com>
2018-09-15 17:14:57 +0100

    describe variable with initializers

--- src/hello/var.go
+++ src/hello/var.go
@@ -3,8 +3,10 @@ package main
 import "fmt"
 
 var c, python, java bool;
+var d int = 2;
+var e, f = 3, 4
 
 func main() {
   var i int;
-  fmt.Println(i, c, python, java)
+  fmt.Println(i, d, e, f, c, python, java)
 }
