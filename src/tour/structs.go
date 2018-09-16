8b2e2737d3dc1013ef69184cdc062d73c4ab2ae7
yogeshagr <yogeshiiith@gmail.com>
2018-09-16 11:36:39 +0100

    describe pointers to struct

--- src/tour/structs.go
+++ src/tour/structs.go
@@ -10,4 +10,13 @@ type Vertex struct {
 func main() {
   fmt.Println(Vertex{2, 3})
   fmt.Println(2)
+
+  v := Vertex{1, 2}
+  v.X = 4
+  fmt.Println(v.X)
+
+  p := &v
+  p.X = 1e9
+  fmt.Println(v)
+
 }
