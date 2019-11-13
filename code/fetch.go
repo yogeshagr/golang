de1b58d2e264f3d6aa5684053e6772a6b5a95bb0
yogeshagr <yogeshiiith@gmail.com>
2019-11-13 18:09:56 +0000

    use copy; add prefix; print status code

--- code/fetch.go
+++ code/fetch.go
@@ -2,24 +2,28 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
+	"strings"
 	"net/http"
 	"os"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
 		resp, error := http.Get(url)
 		if error != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", error)
 			os.Exit(1)
 		}
-		b, error := ioutil.ReadAll(resp.Body)
+		_, error = io.Copy(os.Stdout, resp.Body)
 		resp.Body.Close()
 		if error != nil {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, error)
 			os.Exit(1)
 		}
-		fmt.Printf("%s", b)
+		fmt.Printf("\n%s", resp.Status)
 	}
 }
