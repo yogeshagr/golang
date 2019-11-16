3fdcc79cf8db1da0a94c219d8ee53edc34140abe
yogeshagr <yogeshiiith@gmail.com>
2019-11-16 10:00:27 +0000

    take input via url

--- code/lissajous.go
+++ code/lissajous.go
@@ -9,6 +9,7 @@ import(
 	"io"
 	"math"
 	"math/rand"
+	"strconv"
 )
 
 func main(){
@@ -24,12 +25,21 @@ const (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	lissajous(w)
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+	cycles := 5
+	if cycleStr := r.FormValue("cycles"); cycleStr != "" {
+		if value, err := strconv.Atoi(cycleStr); err == nil {
+			cycles = value
+		}
+	}
+	lissajous(w, cycles)
 }
 
-func lissajous(out io.Writer) {
+func lissajous(out io.Writer, cycles int) {
 	const (
-		cycles = 5   // number of complete x oscillator revolutions
+		//cycles = 5   // number of complete x oscillator revolutions
 		res = 0.001  // angular resolution
 		size = 100   // image canvas covers [-size..+size]
 		nframes = 64 // number of animation frames
@@ -41,7 +51,7 @@ func lissajous(out io.Writer) {
 	for i:= 0; i < nframes; i++ {
 		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
 		img := image.NewPaletted(rect, palette)
-		for t := 0.0; t < cycles*2*math.Pi; t += res {
+		for t := 0.0; t < float64(cycles)*2*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
 			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
