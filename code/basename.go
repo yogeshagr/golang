40ad0c4c5188eff065212c2240b44ff69029f99a
yogeshagr <yogeshiiith@gmail.com>
2019-11-26 18:04:36 +0000

    write a basename function

---
+++ code/basename.go
@@ -0,0 +1,33 @@
+// basename removes the directory components and a .suffix.
+// e.g., a => a, a.go => a, a/b/c.go => c, a/b.c.go => b.c
+
+package main
+
+import (
+	"fmt"
+	)
+
+func basename(s string) string {
+	// Discard last '/' and everything before.
+	for i := len(s) - 1; i >=0; i-- {
+		if s[i] == '/' {
+			s = s[i+1:]
+			break
+		}
+	}
+  // Preserve everything before last '.'.
+	for i := len(s) -1; i >=0; i-- {
+		if s[i] == '.' {
+			s = s[:i]
+			break
+		}
+	}
+	return s
+}
+
+func main(){
+	fmt.Println(basename("a/b/c.go")) 	// "c"
+	fmt.Println(basename("c.d.go")) 		// "c.d"
+	fmt.Println(basename("abc"))    		// "abc"
+	fmt.Println(basename("a/b/c/"))    	// ""
+}
