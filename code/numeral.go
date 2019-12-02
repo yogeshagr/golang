faa9d5d86e42672c337df900d0c570961a23d63f
yogeshagr <yogeshiiith@gmail.com>
2019-12-02 09:42:39 +0000

    write numeral using bytes.Buffer

--- code/numeral.go
+++ code/numeral.go
@@ -6,6 +6,7 @@ package main
 import (
 	"fmt"
 	"strings"
+	"bytes"
 )
 
 func numeral(s string) string {
@@ -16,16 +17,32 @@ func numeral(s string) string {
 	return numeral(s[:n-3]) + "," + s[n-3:]
 }
 
+func numeral2(s string) string {
+	var buf bytes.Buffer
+	n := len(s)
+	pre := n % 3
+	if pre == 0 {
+		pre = 3
+	}
+	buf.WriteString(s[:pre])
+	for i := pre; i < n; i+=3 {
+		buf.WriteByte(',')
+		buf.WriteString(s[i:i+3])
+	}
+	return buf.String()
+}
+
 func main(){
 	fmt.Println("12345 =", numeral("12345"))
 	fmt.Println("1234567 =", numeral("1234567"))
 	fmt.Println("1234 =", numeral("1234"))
 	fmt.Println("123 =", numeral("123"))
-	s := "1234.23"
+	s := "12345.67"
 	if strings.Contains(s, ".") {
 		index := strings.Index(s, ".")
 		fmt.Println(s, numeral(s[:index]) + s[index:])
 	} else {
 		fmt.Println(s, numeral(s))
 	}
+	fmt.Println("1234567", numeral2("1234567"))
 }
