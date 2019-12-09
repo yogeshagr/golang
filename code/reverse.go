cb4d9dc164c257d486463de259fd886e3a6c5b86
yogeshagr <yogeshiiith@gmail.com>
2019-12-09 17:01:09 +0000

    write reverse using array pointer (not recommended in go)

---
+++ code/reverse.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func reverse(a *[6]int) {
+	for i, j := 0, len(*a) - 1; i < j; i, j = i+1, j-1 {
+		a[i], a[j] = a[j], a[i]
+	}
+}
+
+func main() {
+	a := [...]int{0, 1, 2, 3, 4, 5}
+	reverse(&a)
+	fmt.Println(a)
+}
