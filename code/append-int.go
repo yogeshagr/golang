78a4f6ddc7c38efc0ebf1c0281c84e5cc0c98523
yogeshagr <yogeshiiith@gmail.com>
2019-12-09 13:23:05 +0000

    write function to append element to a slice

---
+++ code/append-int.go
@@ -0,0 +1,28 @@
+// Appends an element to the existing slice
+
+package main
+
+import "fmt"
+
+func appendInt(x []int, y int) []int {
+	var z []int
+	zlen := len(x) + 1
+	if zlen <= cap(x) {
+		z = x[:zlen]
+	} else {
+		zcap := zlen
+		if zcap < 2*len(x) {
+			zcap = 2 * len(x)
+		}
+		z = make([]int, zlen, zcap)
+		copy(z, x)
+	}
+	z[len(x)] = y
+	return z
+}
+
+func main() {
+	x := []int{1, 2, 3, 4, 5}
+	y := 6
+	fmt.Println(appendInt(x, y))
+}
