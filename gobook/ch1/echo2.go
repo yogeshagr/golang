f433785afaff25b86bf1fae1385fcd8ad8c54ae7
yogeshagr <yogeshiiith@gmail.com>
2020-05-12 07:47:42 +0100

    add samle go code

---
+++ gobook/ch1/echo2.go
@@ -0,0 +1,16 @@
+// Echo2 prints its command line arguments.
+package main
+
+import (
+	"os"
+	"fmt"
+)
+
+func main() {
+	s, sep := "", ""
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+}
