9e436de04b15d2fce80494a3e68f802f67865b2e
yogeshagr <yogeshiiith@gmail.com>
2020-05-14 06:23:34 +0100

    format

--- gobook/ch1/dup2.go
+++ gobook/ch1/dup2.go
@@ -4,26 +4,26 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
-	"bufio"
 )
 
 func main() {
-	counts := make(map[string]int)
+  counts := make(map[string]int)
 	files := os.Args[1:]
 	if len(files) == 0 {
 		countLines(os.Stdin, counts)
 	} else {
-			for _, file := range files {
-				f, err := os.Open(file)
-				if err != nil {
-					fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
-					continue
-				}
-				countLines(f, counts)
-				f.Close()
+		for _, file := range files {
+			f, err := os.Open(file)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
 			}
+			countLines(f, counts)
+			f.Close()
+		}
 	}
 	for line, count := range counts {
 		if count > 1 {
@@ -32,7 +32,7 @@ func main() {
 	}
 }
 
-func countLines(f *os.File , counts map[string]int) {
+func countLines(f *os.File, counts map[string]int) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
 		counts[input.Text()]++
