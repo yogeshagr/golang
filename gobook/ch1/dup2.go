9286aa9833eb520c853afc7a9527cd082f98e185
yogeshagr <yogeshiiith@gmail.com>
2020-05-14 06:22:04 +0100

    program to compute duplicates from stdin or files

---
+++ gobook/ch1/dup2.go
@@ -0,0 +1,40 @@
+// Dup2 prints the count and text of lines that appear more than once in the
+// input. It reads from stdin or from a list of named files.
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"bufio"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+			for _, file := range files {
+				f, err := os.Open(file)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+					continue
+				}
+				countLines(f, counts)
+				f.Close()
+			}
+	}
+	for line, count := range counts {
+		if count > 1 {
+			fmt.Printf("%d\t%v\n", count, line)
+		}
+	}
+}
+
+func countLines(f *os.File , counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+}
