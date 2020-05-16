bc0dfd1d2bcddf733b2deb415e3aa11005d18d70
yogeshagr <yogeshiiith@gmail.com>
2020-05-16 17:04:19 +0100

    report on headers and form data

--- src/gopl.io/ch1/server2.go
+++ src/gopl.io/ch1/server2.go
@@ -24,6 +24,17 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	count++
 	mu.Unlock()
 	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
+	fmt.Fprintf(w, "Host = %q\n", r.Host)
+	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+	for k, v := range r.Form {
+		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
+	}
 }
 
 // counter echoes the number of calls so far.
