2fb499d69d9308ef5f57987b95cc67a6dd51a33c
yogeshagr <yogeshiiith@gmail.com>
2020-05-16 16:12:49 +0100

    format

--- src/gopl.io/ch1/server2.go
+++ src/gopl.io/ch1/server2.go
@@ -18,6 +18,7 @@ func main() {
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
+// handler echoes the Path component of the requested URL.
 func handler(w http.ResponseWriter, r *http.Request) {
 	mu.Lock()
 	count++
@@ -25,6 +26,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
 }
 
+// counter echoes the number of calls so far.
 func counter(w http.ResponseWriter, r *http.Request) {
 	mu.Lock()
 	fmt.Fprintf(w, "total requests = %d\n", count)
