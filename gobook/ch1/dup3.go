fbde00c3d919edfd94aaba8c2d624c521862829e
yogeshagr <yogeshiiith@gmail.com>
2020-05-16 13:05:10 +0100

    format

--- gobook/ch1/dup3.go
+++ gobook/ch1/dup3.go
@@ -11,7 +11,7 @@ import (
 )
 
 func main() {
-	counts := make(map[string]int)
+	counts := make(map [string]int)
 	for _, filename := range os.Args[1:] {
 		data, err := ioutil.ReadFile(filename)
 		if err != nil {
