f6bf6117b5ea67664d9ab898820b80f4b0a53aca
yogeshagr <yogeshiiith@gmail.com>
2020-05-18 07:24:02 +0100

    define flag example

---
+++ src/gopl.io/ch2/flag.go
@@ -0,0 +1,22 @@
+// Flag prints its command-line arguments.
+
+package main
+
+import (
+	"fmt"
+	"flag"
+	"strings"
+)
+
+// The variables sep and n are pointers to the flag variables, which must be
+// accessed indirectly as *sep and *n
+var n = flag.Bool("n", false, "omit trailing newline")
+var sep = flag.String("s", " ", "separator")
+
+func main() {
+	flag.Parse()
+	fmt.Println(strings.Join(flag.Args(), *sep))
+	if !*n {
+		fmt.Println()
+	}
+}
