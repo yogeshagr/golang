11e2f9590681be38b9d423806f74458a14cb37e2
yogeshagr <yogeshiiith@gmail.com>
2020-05-22 18:24:38 +0100

    compute contains

---
+++ src/gopl.io/ch2/substring.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func hasPrefix(s, prefix string) bool {
+	return len(s) >= len(prefix) && s[:len(prefix)] == prefix
+}
+
+func hasSuffix(s, suffix string) bool {
+	return len(s) >= len(suffix) && s[len(s)-len(suffix):] == suffix
+}
+
+func contains(s, substr string) bool {
+	for i := 0; i < len(s); i++ {
+		if hasPrefix(s[i:], substr) {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	s := "abcde"
+	substr := "bc"
+	fmt.Println(contains(s, substr))
+}
