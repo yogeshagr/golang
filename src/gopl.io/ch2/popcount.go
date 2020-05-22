2f66949b6a7e39bfe5ce4c6482e75f57fcb44ed0
yogeshagr <yogeshiiith@gmail.com>
2020-05-22 07:22:12 +0100

    compare performance of the functions

---
+++ src/gopl.io/ch2/popcount.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+func PopCount1(x uint64) int {
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+}
+
+func PopCount2(x uint64) int {
+	var temp int
+	for i := 0; i < 8; i++ {
+		temp += int(pc[byte(x>>(i*8))])
+	}
+	return temp
+}
+
+func main() {
+	fmt.Printf("%v\n", PopCount1(10000))
+}
