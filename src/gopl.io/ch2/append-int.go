ab4e8d6e8ce14803c2400dab4197cf01fd4f532c
yogeshagr <yogeshiiith@gmail.com>
2020-05-23 21:49:52 +0100

    write append int function

---
+++ src/gopl.io/ch2/append-int.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func appendInt(x []int, y int) []int {
+	var z []int
+	zlen := len(x) + 1
+	if zlen <= cap(x) {
+		// There is room to grow. Extend the slice.
+		z = x[:zlen]
+	} else {
+		// There is insufficient space. Allocate a new array.
+		// Grow by doubling, for amortized linear complexity.
+		zcap := zlen
+		if zcap < 2*len(x) {
+			zcap = 2 * len(x)
+		}
+		z = make([]int, zlen, zcap)
+		copy(z, x)
+	}
+	z[len(x)] = y
+	return z
+}
+
+func main() {
+	x := []int{1, 2, 3}
+	y := appendInt(x, 4)
+	fmt.Printf("%v\n", y)
+}
