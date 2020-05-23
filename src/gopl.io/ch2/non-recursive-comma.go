1a770542598fec3da3addbc8dfc75904af8ea50b
yogeshagr <yogeshiiith@gmail.com>
2020-05-23 09:34:42 +0100

    enhance comma to handle float cases

--- src/gopl.io/ch2/non-recursive-comma.go
+++ src/gopl.io/ch2/non-recursive-comma.go
@@ -3,22 +3,40 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"strings"
 )
 
 func comma(s string) string {
 	var buf bytes.Buffer
-	pre := len(s) % 3
+
+	start := 0
+	end := len(s)
+	if s[0] == '-' || s[0] == '+' {
+		buf.WriteByte('-')
+		start = 1
+	}
+
+	float := false
+	dot := -1
+	if dot = strings.Index(s, "."); dot > 0 {
+		float = true
+		end = dot
+	}
+	pre := (end - start) % 3
 	if pre == 0 {
 		pre = 3
 	}
-	buf.WriteString(s[:pre])
-	for i := pre; i < len(s); i += 3 {
+	buf.WriteString(s[start : start+pre])
+	for i := start + pre; i < end; i += 3 {
 		buf.WriteByte(',')
 		buf.WriteString(s[i : i+3])
+	}
+	if float {
+		buf.WriteString(s[dot:])
 	}
 	return buf.String()
 }
 
 func main() {
-	fmt.Printf("%v\n", comma("1234556"))
+	fmt.Printf("%v\n", comma("1234556.9791872"))
 }
