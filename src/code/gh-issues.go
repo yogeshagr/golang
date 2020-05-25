aab3e7c68207d537d8b2b0cd5be167ae00e857b9
yogeshagr <yogeshiiith@gmail.com>
2020-05-25 10:05:32 +0100

    use marshalling to populate json object

--- src/code/gh-issues.go
+++ src/code/gh-issues.go
@@ -3,26 +3,41 @@
 package main
 
 import (
-	"net/http"
-	"fmt"
 	"bytes"
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
+	"net/http"
 	"os"
 	"strings"
-	)
+)
 
 const APIURL = "https://api.github.com"
 
+type issue struct {
+	Title     string   `json:"title"`
+	Body      string   `json:"body"`
+	Labels    []string `json:"labels"`
+	Assignees []string `json:"assignees"`
+}
+
 func createIssue(owner string, repo string) {
 	url := strings.Join([]string{APIURL, "repos", owner, repo, "issues"}, "/")
-	var jsonStr = []byte(`{
+	var myIssue issue
+	myIssue.Title = "Found a bug"
+	myIssue.Body = "I'm having a problem with this"
+	myIssue.Labels = []string{"bug"}
+	myIssue.Assignees = []string{"yogeshagr"}
+	myJson, err := json.Marshal(myIssue)
+
+	/*	var jsonStr := []byte(`{
 		"title": "Found a bug",
 		"body": "I'm having a problem with this",
 		"labels": ["bug"],
 		"assignees": ["yogeshagr"]
-		}`)
+		}`) */
 
-  req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(myJson))
 	req.Header.Set("X-Custom-Header", "myvalue")
 	req.Header.Set("Content-Type", "application/json")
 	req.SetBasicAuth(os.Getenv("GITHUB_USER"), os.Getenv("GITHUB_PASS"))
@@ -36,7 +51,7 @@ func createIssue(owner string, repo string) {
 	fmt.Println("response status:", resp.Status)
 	fmt.Println("response headers", resp.Header)
 	body, _ := ioutil.ReadAll(resp.Body)
-  fmt.Println("response body", string(body))
+	fmt.Println("response body", string(body))
 }
 
 var usage string = `usage:
