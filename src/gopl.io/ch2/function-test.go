96c8953566c7addb4d242dc1c5b43b06fa177c24
yogeshagr <yogeshiiith@gmail.com>
2020-05-25 17:27:36 +0100

    return function from function

--- src/gopl.io/ch2/function-test.go
+++ src/gopl.io/ch2/function-test.go
@@ -8,6 +8,17 @@ func a(x int, y int) {
 func b(y int, x int) {
 }
 
+func embedded() func() int {
+	x := 0
+	square := func() int {
+		x++
+		return x * x
+	}
+	return square
+}
+
 func main() {
 	fmt.Printf("%T\n%T\n", a, b)
+	f := embedded()
+	fmt.Println(f())
 }
