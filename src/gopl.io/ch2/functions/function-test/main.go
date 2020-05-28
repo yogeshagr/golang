b0b538e96f0ad57cd2d8e5a08fc661e3b8168ed4
yogeshagr <yogeshiiith@gmail.com>
2020-05-28 22:29:14 +0100

    test lexical scope

--- src/gopl.io/ch2/functions/function-test/main.go
+++ src/gopl.io/ch2/functions/function-test/main.go
@@ -17,9 +17,20 @@ func embedded() func() int {
 	return square
 }
 
+func test() {
+	a := 10
+	fmt.Println(a)
+
+	{
+		a := 20
+		fmt.Println(a)
+	}
+}
+
 func main() {
 	fmt.Printf("%T\n%T\n", a, b)
 	f := embedded()
 	fmt.Println(f())
 	fmt.Println(f())
+	test()
 }
