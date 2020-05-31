a39e8b1dffaff00ffb527854c2f7c90bcf3bca7c
yogeshagr <yogeshiiith@gmail.com>
2020-05-31 07:36:04 +0100

    restructure

--- src/code/helloworld.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Println("नमस्ते दुनिया")
-}
--- src/code/unsigned-int.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	var a uint = 0
-	fmt.Println(a - 1) // Prints maximum uint value (for example, 2^64 - 1) 
-}
