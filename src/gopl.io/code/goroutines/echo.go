abe92d4004a33b7450792d0f6952a9cba7972c5a
yogeshagr <yogeshiiith@gmail.com>
2020-05-31 12:55:20 +0100

    add goroutine examples

---
+++ src/gopl.io/code/goroutines/echo.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+	"time"
+)
+
+func echo(c net.Conn, shout string, delay time.Duration) {
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+}
+
+func handleFunc(c net.Conn) {
+	input := bufio.NewScanner(c)
+	for input.Scan() {
+		go echo(c, input.Text(), 1*time.Second)
+	}
+	c.Close()
+}
+
+func main() {
+	port := os.Args[1]
+	address := fmt.Sprintf("%s:%s", "localhost", port)
+	fmt.Println(address)
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err) // e.g., connection aborted
+			continue
+		}
+		go handleFunc(conn) // handle one connection at a time
+	}
+}
---
+++ src/gopl.io/code/goroutines/netcat1-channel.go
@@ -0,0 +1,42 @@
+/*
+The netcat1 program copies input to the server in its main gorotine, so the
+client program terminates as soon as the input stream closes, even if the
+background goroutine is still working. To make the program wait for the
+background goroutine to complete before exiting, we use a channel to synchronize
+the two goroutines.
+*/
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+	done := make(chan struct{})
+	go func() {
+		io.Copy(os.Stdout, conn) // NOTE: ignoring errors
+		log.Println("done")
+		done <- struct{}{} // Signal the main goroutine
+	}()
+
+	mustCopy(conn, os.Stdin)
+	conn.Close()
+	<-done // wait for background goroutine to finish
+}
+
+func mustCopy(dst io.Writer, src io.Reader) {
+	fmt.Println("copying to server")
+	if _, err := io.Copy(dst, src); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("copying to server done")
+}
